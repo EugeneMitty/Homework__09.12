2499aa99eadd0770deef7f9b52a7f1b910ffab93
Eugene <97278341+EugeneMtty@users.noreply.github.com>
2022-12-14 10:42:14 +0300

    Решение

---
+++ Zadacha__№43/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите k1: ");
+        double k1 = double.Parse(Console.ReadLine());
+        Console.Write("Введите b1: ");
+        double b1 = double.Parse(Console.ReadLine());
+        Console.Write("Введите k2: ");
+        double k2 = double.Parse(Console.ReadLine());
+        Console.Write("Введите b2: ");
+        double b2 = double.Parse(Console.ReadLine());
+        double x = (b1 - b2) / (k2 - k1);
+        double y = (k2 * x + b2);
+        Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
+    }
+}
