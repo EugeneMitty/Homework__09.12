8cbb840abe3d0c66d068b433d43599ca4de83811
Eugene <97278341+EugeneMtty@users.noreply.github.com>
2022-12-14 10:40:59 +0300

    Решение

--- Zadacha__№41/Program.cs
+++ Zadacha__№41/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[] tempo(string[] strings)
+{
+    double[] numbers = new double[strings.Length];
+
+    for (int i = 0; i < strings.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(strings[i]);
+    }
+    return numbers;
+}
+int countPositive(double[] numbers)
+{
+    int count = 0;
+    foreach (var number in numbers)
+    {
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите некоторое количество(М) чисел, разделяя их пробелами");
+
+string[] strNumbers = Console.ReadLine().Split(" ");
+
+try
+{
+    double[] numbers = tempo(strNumbers);
+    Console.WriteLine("Количество чисел больше 0: " + countPositive(numbers));
+}
+catch (Exception e)
+{
+    Console.WriteLine("Некорректное значение");
+}
